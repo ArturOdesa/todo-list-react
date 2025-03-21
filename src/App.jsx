85092060f96033cae5095422ae0c5fd6347b2f1f
Artur Stoianov <stoianovart@gmail.com>
2025-03-21 12:24:04 +0200

    add .main block

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import styles from './App.module.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
+      <main className={styles.main}>
+        <header className={styles.main__header}>
+          <div className={styles.container}>
+            <div className={styles.header__top}>
+              <h1 className={styles.title}>Note your tasks</h1>
+              <p className={styles.date}>Date</p>
+            </div>
+            <div className={styles.header__bottom}>
+              <input type="text" placeholder='Task name' />
+              <button>+</button>
+            </div>
+          </div>
+        </header>
+        <section>
+          
+        </section>
+      </main>
     </>
   )
 }
