5538ca61d82463e2bf32647d6f70de8bd2cc6a04
Artur Stoianov <stoianovart@gmail.com>
2025-03-22 01:11:06 +0200

    improve codestyle and remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,50 @@
-import styles from './App.module.scss'
+import styles from './App.module.scss';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import { v4 as uuid4v } from 'uuid';
+import List from './containers/List/List';
+
 
 function App() {
+  
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      return JSON.parse(savedTasks);
+    } else {
+      return [];
+    }
+  });
+  const [taskName, setTaskName] = useState('');
+
+  const date = new Date().toLocaleDateString('en-En', { day: 'numeric', month: 'long', year: 'numeric' });
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTaskHandler = () => {
+    if (taskName.trim()) {
+      const newTask = {
+        'id': uuid4v(),
+        'name': taskName.trim(),
+        'completed': false,
+      };
+      setTasks([...tasks, newTask]);
+      setTaskName('');
+    }
+  };
+
+  const removeTaskHandler = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const changeStateHandler = (id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    ));
+  }
 
   return (
     <>
@@ -9,20 +53,30 @@ function App() {
           <div className={styles.container}>
             <div className={styles.header__top}>
               <h1 className={styles.title}>Note your tasks</h1>
-              <p className={styles.date}>Date</p>
+              <p className={styles.date}>{date}</p>
             </div>
             <div className={styles.header__bottom}>
-              <input type="text" placeholder='Task name' />
-              <button>+</button>
+              <input type="text" placeholder='Task name' value={taskName} onChange={(e) => {
+                setTaskName((e.target.value));
+              }}
+              onKeyDown={(e) => {
+                e.key === 'Enter' && addTaskHandler();
+              }}/>
+              <button onClick={(addTaskHandler)}>+</button>
             </div>
           </div>
         </header>
         <section>
-          
+          <div className={styles.container}>
+            <p hidden={tasks.length}>No any tasks...</p>
+            <List tasks={tasks} changeState={changeStateHandler} removeTask={removeTaskHandler} />
+          </div>
         </section>
       </main>
     </>
   )
 }
+
+// 
 
 export default App
